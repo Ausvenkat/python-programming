295585a2573dfa63dbfeb77976a21b03a77cdba7
Ausvenkat <36623888+Ausvenkat@users.noreply.github.com>
2018-05-17 09:45:14 +0530

    Update ex5.py

--- Beginner Level/ex5.py
+++ Beginner Level/ex5.py
@@ -1,11 +1,11 @@
-a=input()
-b=input()
-c=input()
-if(a>b and a>c):
-     print(a)
-elif(b>c and b>a):
-     print(b)
-elif(c>a and c>b):
-     print(c)
+v=int(input())
+s=int(input())
+t=int(input())
+if (v>=s and v>=t):
+     print(v)
+elif (s>=t and s>=v):
+     print(s)
+elif (t>=v and t>=s):
+     print(t)
 else:
-     print("Invalid Input")
+     print("InvaildInput")

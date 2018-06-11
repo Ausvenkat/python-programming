37a3662bea2763fd4dd676ed7f3c15be27bd7a5d
Ausvenkat <36623888+Ausvenkat@users.noreply.github.com>
2018-06-10 23:20:22 -0700

    Update and rename maximum.py to maxele.py

--- Beginner Level/maxele.py
+++ Beginner Level/maxele.py
@@ -1,10 +1,10 @@
 try:
-	a=[]
+	v=[]
 	n=int(input())
 	for i in range(1,n+1):
 	    b=int(input())
-	    a.append(b)
-	a.sort()
-	print(a[n-1])
+	    v.append(b)
+	v.sort()
+	print(v[n-1])
 except:
 	print("InvalidInput")

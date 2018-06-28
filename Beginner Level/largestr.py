ad02133da5d64fa63b426f3322189b9ef8d6317e
Ausvenkat <36623888+Ausvenkat@users.noreply.github.com>
2018-06-27 22:24:16 -0700

    Update largestr.py

--- Beginner Level/largestr.py
+++ Beginner Level/largestr.py
@@ -7,9 +7,8 @@ for i in string1:
 for j in string2:
       count2=count2+1
 if(count1<count2):
-      print("Larger string is:")
       print(string2)
 elif(count1==count2):
-      print("Both strings are equal.")
+      print(string2)
 else:
       print(string1)

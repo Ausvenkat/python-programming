6fc762682b6d47856d4bb6122093c072d20c0a52
Ausvenkat <36623888+Ausvenkat@users.noreply.github.com>
2019-07-07 21:13:26 +0530

    Create arrindex.cpp

---
+++ Beginner Level/arrindex.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+     int o,n;
+     cin>>n;
+     int k[o];
+     for(int i=0;i<n;i++){
+     	cin>>k[i];
+     }
+     for(int i=0;i<n;i++){
+     cout<<k[i]<<"\t"<<i<<"\t";
+     }
+}

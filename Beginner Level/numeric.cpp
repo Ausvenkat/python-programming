c0f4e2ed9d62047ee39791ecc9652075c0ebe375
Ausvenkat <36623888+Ausvenkat@users.noreply.github.com>
2019-07-09 23:02:42 +0530

    Create numeric.cpp

---
+++ Beginner Level/numeric.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string s;
+	int count=0;
+	cin>>s;
+	for(int i=0;i<s.length();i++){
+		if(s[i]=='.' || (s[i]>='0'&& s[i]<='9') ){
+			count++;
+		}
+		
+	}
+	if(count==s.length()){
+		cout<<"yes";
+	}	else{
+		cout<<"no";
+	}
+
+}

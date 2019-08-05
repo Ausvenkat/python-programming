d86f4bbff74fd5c36911ab86684f0ca52ba3a5a5
Ausvenkat <36623888+Ausvenkat@users.noreply.github.com>
2019-08-05 22:09:21 +0530

    Create printOddDigit.cpp

---
+++ Beginner Level/printOddDigit.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+	string s;
+	cin>>s;
+	for(int i=0;i<s.length();i++){
+		if(s[i]%2!=0){
+			cout<<s[i]<<' ';
+		}
+	}
+    return 0;
+}

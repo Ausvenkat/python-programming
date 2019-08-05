0dc3d4c92cc732371b72a7a33f649e06016cc4f3
Ausvenkat <36623888+Ausvenkat@users.noreply.github.com>
2019-08-05 20:34:10 +0530

    Update oddinterval.py

--- Beginner Level/oddinterval.py
+++ Beginner Level/oddinterval.py
@@ -6,3 +6,20 @@ try:
 	          print(i)
 except:
 	print("InvalidInput")
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b,n;
+	cin>>a>>b;
+	for(int i=a;i<b-1;i++){
+		n=i+1;
+		if(n%2!=0){
+			cout<<n<<' ';
+		}
+	}
+	return 0;
+}

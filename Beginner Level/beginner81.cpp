426f90b5a2612d5012fe72c839be663cc8129de6
Ausvenkat <36623888+Ausvenkat@users.noreply.github.com>
2019-08-06 21:33:05 +0530

    Create beginner81.cpp

---
+++ Beginner Level/beginner81.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main() 
+{
+	int v,t,s;
+ while (!cin.eof())
+{
+    cin >> v >>t;
+    s=t-v;
+    cout <<s<< endl;
+} 
+    
+    return 0;
+}

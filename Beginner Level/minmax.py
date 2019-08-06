f5c3e1f742f825b7f84d1ea16afe4de3ceb9389d
Ausvenkat <36623888+Ausvenkat@users.noreply.github.com>
2019-08-06 18:09:20 +0530

    Update minmax.py

--- Beginner Level/minmax.py
+++ Beginner Level/minmax.py
@@ -4,3 +4,29 @@ for n in range(num):
     numbers = int(input())
     a.append(numbers)
 print( max(a), min(a))
+
+
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int i,m,n;
+    cin>>n;
+    int arr[n];
+    for (i = 0; i <n; i++)
+        cin >> arr[i];
+    m = arr[0];
+    for (i = 0; i <n; i++)
+    {
+        if (m > arr[i])
+            m = arr[i];
+    }
+    cout<<m<<' ';
+    for (i = 0; i <n; i++)
+    {
+        if (m < arr[i])
+            m = arr[i];
+    }
+    cout<<m;
+        return 0;
+}

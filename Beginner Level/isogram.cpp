3920dcb53045a5edff119f6d70f6b13143fd8639
Ausvenkat <36623888+Ausvenkat@users.noreply.github.com>
2019-08-06 13:21:25 +0530

    Create isogram.cpp

---
+++ Beginner Level/isogram.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string s;
+	int count=0;
+	cin>>s;
+	int l=s.length();
+	for(int i=0;i<l;i++){
+		if(s[i]!=s[i+1]){
+			count++;
+		}
+		
+	}
+	if(count==l){
+		cout<<"yes";
+	}
+	else{
+		cout<<"no";
+	}
+}

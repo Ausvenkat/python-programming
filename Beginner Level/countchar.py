67dbebaaf35df945dd187b704ff24959c2726ff8
Ausvenkat <36623888+Ausvenkat@users.noreply.github.com>
2019-08-06 12:57:31 +0530

    Update countchar.py

--- Beginner Level/countchar.py
+++ Beginner Level/countchar.py
@@ -5,3 +5,21 @@ for i in string:
       if(i!=' '):
            char=char+1      
 print(char)
+
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int count=0;
+	string s;
+	getline(cin,s);
+	for(int i=0;i<s.length();i++){
+		if(s[i]!=' '){
+			count++;
+		}
+	}
+	cout<<count;
+}

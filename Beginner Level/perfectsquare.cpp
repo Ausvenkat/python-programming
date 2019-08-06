876337a7b9cb92730bba70a7b09dc800d12d6aea
Ausvenkat <36623888+Ausvenkat@users.noreply.github.com>
2019-08-06 21:47:27 +0530

    Create perfectsquare.cpp

---
+++ Beginner Level/perfectsquare.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main() 
+{
+	int a,b,c,count=0;
+	cin>>a>>b;
+	c=a*b;
+	for(int i=1;i<=c;i++){
+		if(c==i*i){
+			count++;
+		}
+	}
+    if(count>0){
+    	cout<<"yes";
+    }
+    else{
+    	cout<<"no";
+    }
+    return 0;
+}

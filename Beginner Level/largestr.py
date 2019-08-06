b03e60241722c1416cf33af2fff11b7a43f05451
Ausvenkat <36623888+Ausvenkat@users.noreply.github.com>
2019-08-06 16:57:54 +0530

    Update largestr.py

--- Beginner Level/largestr.py
+++ Beginner Level/largestr.py
@@ -12,3 +12,29 @@ elif(count1==count2):
       print(string2)
 else:
       print(string1)
+
+      
+      
+      #include<iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+   string s;
+   string k;
+   int count1=0,count2=0;
+   cin>>s>>k;
+   for(int i=0;i<s.length();i++){
+   	count1++;
+   }
+   for(int j=0;j<k.length();j++){
+   	count2++;
+   }
+   if(count1>=count2){
+   	cout<<s;
+   }
+   else{
+   	cout<<k;
+   }
+   
+}

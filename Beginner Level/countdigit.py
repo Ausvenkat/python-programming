0c819622abc7d2520e28c41fb9d37d09dd8e22da
Ausvenkat <36623888+Ausvenkat@users.noreply.github.com>
2019-08-06 17:39:44 +0530

    Update countdigit.py

--- Beginner Level/countdigit.py
+++ Beginner Level/countdigit.py
@@ -4,3 +4,18 @@ while(v>0):
     count1=count1+1
     v=v//10
 print(count1)
+
+
+#include<iostream>
+#include <string>
+
+using namespace std;
+int main ()
+{
+	string s;
+	int count=0;
+	cin>>s;
+	cout<<s.length();
+	
+   
+}

0701d565da1331a2fab926b4cf3a87211bd10543
Ausvenkat <36623888+Ausvenkat@users.noreply.github.com>
2019-08-07 08:19:06 +0530

    Update ChangePosInOreder.cpp

--- Beginner Level/ChangePosInOreder.cpp
+++ Beginner Level/ChangePosInOreder.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main()
 {
-	int n;
-	cin>>n;
-	int a[n];
-	for(int i=0;i<n;i++){
+	int no;
+	cin>>no;
+	int a[no];
+	for(int i=0;i<no;i++){
 		cin>>a[i];
 	}
-	for(int i=0;i<n;i++){
+	for(int i=0;i<no;i++){
 		if(a[i]>a[i+1]){
 			cout<<i;		
 		}

2760858eff895097baf99248ccb0d0e430a30768
Ausvenkat <36623888+Ausvenkat@users.noreply.github.com>
2019-08-07 20:51:30 +0530

    Update set21.py

--- Beginner Level/set21.py
+++ Beginner Level/set21.py
@@ -9,3 +9,21 @@ try:
 	print(power(base,exp))
 except:
   print("InvalidInput")
+
+
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int exponent;
+    float base, result = 1;
+    cin >> base >> exponent;
+    while (exponent != 0) {
+        result *= base;
+        --exponent;
+    }
+    cout << result;
+    
+    return 0;
+}
